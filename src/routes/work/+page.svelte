8c70d8938f2893b3d947789681aafa1a566635bf
Jared Scarr <jaredscarr@pm.me>
2024-03-23 10:08:31 -0700

    Replaced carousel with grid layout.

--- src/routes/work/+page.svelte
+++ src/routes/work/+page.svelte
@@ -3,45 +3,49 @@
   import { fly } from 'svelte/transition';
   import { backOut } from "svelte/easing";
   import { onMount } from "svelte";
-  import { ArrowBigRight, ArrowBigLeft } from "lucide-svelte";
-
 
   const projects = [
     {
       id: 1,
+      title: "Deck.gl Contribution",
+      url: "https://github.com/visgl/deck.gl/pull/8545",
+      imgUrl: "arcgis-screenshot-1.png",
+    },
+    {
+      id: 2,
+      title: "Swiss Tournament Style API",
+      url: "https://github.com/jaredscarr/swiss-tournament",
+      imgUrl: "swiss-tournament.png",
+    },
+    {
+      id: 3,
       title: "Sandra Creative Artist Blog",
       url: "https://sandracreative.com",
       imgUrl: "sandracreative.png",
     },
     {
-      id: 2,
+      id: 4,
       title: "Computer Science Club Website",
       url: "https://northcs.org",
       imgUrl: "northcs.png",
     },
     {
-      id: 3,
+      id: 5,
       title: "Computer Science Club Workbook",
       url: "https://nsccs.github.io/",
       imgUrl: "workbook.png",
     },
     {
-      id: 4,
+      id: 6,
       title: "Retro Games Open Collaboration",
       url: "https://github.com/nsccs/pygames",
       imgUrl: "conways.png",
     },
     {
-      id: 5,
+      id: 7,
       title: "Page Replacement Experiment",
       url: "https://github.com/jaredscarr/page-replacement",
       imgUrl: "page-replacement.png",
-    },
-    {
-      id: 6,
-      title: "Swiss Tournament Style API",
-      url: "https://github.com/jaredscarr/swiss-tournament",
-      imgUrl: "swiss-tournament.png",
     }
   ]
 
@@ -49,23 +53,6 @@
   onMount(() => {
     animate = true;
   });
-
-
-  let elemProjects: HTMLDivElement;
-
-  function multiColumnLeft(): void {
-    let x = elemProjects.scrollWidth;
-    if (elemProjects.scrollLeft !== 0) x = elemProjects.scrollLeft - elemProjects.clientWidth;
-    elemProjects.scroll(x, 0);
-  }
-
-  function multiColumnRight(): void {
-    let x = 0;
-    // -1 is used because different browsers use different methods to round scrollWidth pixels.
-    if (elemProjects.scrollLeft < elemProjects.scrollWidth - elemProjects.clientWidth - 1) x = elemProjects.scrollLeft + elemProjects.clientWidth;
-    elemProjects.scroll(x, 0);
-  }
-
 
 </script>
 
@@ -83,44 +70,23 @@
       Work
     </h2>
     <p class="mb-10 tracking-wide leading-relaxed">Not all my work is in the public domain. This is collection of my personal work that includes applications, group projects, university course work, and documentation. Thanks for looking!</p>
-
-    <div class="grid grid-cols-[auto_1fr_auto] gap-1 items-center">
-      <!-- Carousel -->
-      <div bind:this={elemProjects} class="snap-x snap-mandatory scroll-smooth flex gap-2 pb-2 overflow-x-auto mb-4">
-        {#each projects as project}
-          <div class="snap-start shrink-0 card-hover variant-glass-surface py-0 w-72 md:w-80 text-center">
-            <header class="card-header mb-1">
-              <h5 class="h5 text-tertiary-500-400-token">{project.title}</h5>
-            </header>
-            <section class="p-2 mb-2">
-              <img src="{project.imgUrl}" alt="Project Landing Page">
-            </section>
-            <footer class="card-footer text-right">
-              <a
-                class="btn variant-filled-secondary rounded-full"
-                href="{project.url}"
-                data-sveltekit-preload-data="hover"
-                target="_blank"
-              >
-                Check it Out!
-              </a>
-            </footer>
-          </div>
-        {/each}
-      </div>
-    </div>
-    <div class="container flex mb-20">
-      <div>
-        <button type="button" class="btn-icon variant-filled" on:click={multiColumnLeft}>
-          <ArrowBigLeft />
-        </button>
-      </div>
-      <div class="ml-auto">
-        <button type="button" class="btn-icon variant-filled" on:click={multiColumnRight}>
-          <ArrowBigRight />
-        </button>
-      </div>
-    </div>
+    <section class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-20">
+      {#each projects as project}
+      <a
+        class="block card card-hover p-1"
+        href={project.url}
+        data-sveltekit-preload-data="hover"
+        target="_blank"
+      >
+        <header class="card-header mb-1">
+          <h5 class="h5 text-tertiary-500-400-token">{project.title}</h5>
+        </header>
+        <section class="p-2 mb-1">
+          <img src="{project.imgUrl}" alt="Project Landing Page">
+        </section>
+      </a>
+      {/each}
+    </section>
   </div>
   {/if}
   <Logo />
