ebefd25b738615ac633347834af3edaf107e53c6
Jared Scarr <jaredscarr@pm.me>
2024-04-03 14:43:17 -0700

    Changed sub-title.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,16 +8,16 @@
 </svelte:head>
 
 <section class="grid grid-cols-1 m-10 mt-20 gap-5 sm:grid-cols-2 sm:gap-10 sm:m-20">
-  <div class="m-0 sm:m-20 tracking-wide">
-    <h1 class="h1 mb-8 font-bold tracking-widest text-primary-900-50-token">
+  <div class="m-0 sm:m-30">
+    <h1 class="h1 mt-40 mb-20 font-bold tracking-widest text-primary-900-50-token">
       JARED SCARR
     </h1>
-    <p class="mb-6 tracking-wide">
-      I work as a Software Development Engineer based out of Seattle, Washington.
+    <p class="mb-36 font-bold tracking-wide">
+      Full Stack Software Development Engineer based out of Seattle, Washington.
     </p>
     <a class="btn btn-sm variant-filled-secondary font-extrabold" href="https://www.linkedin.com/in/jaredscarr" data-sveltekit-preload-data="hover">Connect</a>
   </div>
-  <div class="text-justify m-0 sm:m-20">
+  <div class="m-0 sm:m-20">
     <img class="h-auto max-w-full visible" src="main-image-light.png" alt="Man with laptop">
   </div>
 </section>
