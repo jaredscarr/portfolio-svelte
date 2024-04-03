f8e731b621f80196d73b2f39856a91088cf6b624
Jared Scarr <jaredscarr@pm.me>
2024-04-03 13:54:40 -0700

    Removal of unused images, removed generic logo, added headshot to about page, added linked in button.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -29,8 +29,8 @@
       </div>
       <svelte:fragment slot="trail">
         <LightSwitch />
-        <a href="https://github.com/jaredscarr" class="btn-icon" data-sveltekit-preload-data="hover"><Github /></a>
-        <a href="https://www.linkedin.com/in/jaredscarr" class="btn-icon" data-sveltekit-preload-data="hover"><Linkedin /></a>
+        <a href="https://github.com/jaredscarr" class="btn-icon hover:bg-secondary-400-500-token" data-sveltekit-preload-data="hover"><Github /></a>
+        <a href="https://www.linkedin.com/in/jaredscarr" class="btn-icon hover:bg-secondary-400-500-token" data-sveltekit-preload-data="hover"><Linkedin /></a>
       </svelte:fragment>
     </AppBar>
     <Drawer>
